08cd9a072474832b3301df5539efa9d707abf932
Екатерина Бирюкова <2019103253@pnu.edu.ru>
2021-03-10 19:14:02 +1000

    added classs

--- Project10/Source.cpp
+++ Project10/Source.cpp
@@ -1,4 +1,26 @@
-#include <iostream>
-int main() {
-	std::cout << "Hello world";
-}
+#include <algorithm>
+#include <vector>
+
+
+class FunctionPart {
+public:
+    FunctionPart(char new_operation, double new_value) {
+        operation = new_operation;
+        
+    }
+    double Apply(double source_value) const {
+        if (operation == '+') {
+            return source_value + value;
+        }
+     
+    }
+    void Invert() {
+        if (operation == '+') {
+            operation = '-';
+        }
+
+    }
+private:
+    char operation;
+    double value;
+};
