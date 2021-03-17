f0776c6540c5519df6be0443c5142a066f2a28fc
AnBch21 <79948612+AnBch21@users.noreply.github.com>
2021-03-17 11:26:17 +1000

    Revert "Function"

--- Project10/Source.cpp
+++ Project10/Source.cpp
@@ -1,26 +1,4 @@
-#include <algorithm>
-#include <vector>
+#include <iostream>
+int main() {
 
-
-class FunctionPart {
-public:
-    FunctionPart(char new_operation, double new_value) {
-        operation = new_operation;
-        
-    }
-    double Apply(double source_value) const {
-        if (operation == '+') {
-            return source_value + value;
-        }
-     
-    }
-    void Invert() {
-        if (operation == '+') {
-            operation = '-';
-        }
-
-    }
-private:
-    char operation;
-    double value;
-};
+}
